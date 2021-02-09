7a928d897d979d81ad2412cfc0e2889553854cff
mfary <moha1379>
2021-02-09 20:40:58 +0330

    function class

---
+++ src/table.py
@@ -0,0 +1,6 @@
+class FuncDef:
+    def __init__(self, name, lable, inputs_type, return_type):
+        self.name = name
+        self.lable = lable
+        self.inputs_type = inputs_type
+        self.return_type = return_type
