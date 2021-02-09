bd50ef967c314df04a1f017aa5f1cb298802bb56
mfary <moha1379>
2021-02-09 20:47:24 +0330

    declare class type

--- src/table.py
+++ src/table.py
@@ -1,6 +1,18 @@
+import enum
+
+
 class FuncDef:
     def __init__(self, name, lable, inputs_type, return_type):
         self.name = name
         self.lable = lable
         self.inputs_type = inputs_type
         self.return_type = return_type
+
+
+class PrimitiveType(enum.Enum):
+    integer = "int"
+    double = "double"
+    string = "string"
+    boolean = "bool"
+    array = "array"
+    null = "null"
