41611b4fbdb0d0a96802bf570c3fe3f99ebd63b3
mfary <moha1379>
2021-02-14 15:47:36 +0330

    add some basic emits

--- src/mipsCodes.py
+++ src/mipsCodes.py
@@ -3,8 +3,52 @@ debug = True
 
 def emit_comment(comment):
     if debug:
-        emit("#" + comment)
+        print("#" + comment)
 
 
 def emit(line):
     print(line)
+
+
+def emit_lable(label):
+    print(lable + " :")
+
+
+def emit_add(output, input1, input2):
+    print("add " + output + ", " + input1 + ", " + input2)
+
+
+def emit_addi(output, input1, imediete):
+    print("addi " + output + ", " + input1 + ", " + imediete)
+
+
+def emit_syscal():
+    print("syscall")
+
+
+def emit_jal(lable):
+    print("jal " + lable)
+
+def emit_l(lable):
+    print("j " + lable)
+
+
+def emit_jalr(register):
+    emit("jalr", register)
+
+def emit_move(destination, source):
+
+
+def emit_la(destination, lable):
+    print("la " + destination + ", " + lable)
+
+
+def emit_lw(destination, source, offset = 0, word = True):
+    if word:
+        print("lw " + destination + ", " + str(offset) + "(" + source + ")")
+    else :
+        print("lb " + destination + ", " + str(offset) + "(" + source + ")")
+
+
+def emit_li(destination, imediate):
+    print("li " + destination + ", " + imediate)
