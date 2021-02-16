d5160af1f48cb9fa2abf2e006466615e380e906f
mfary <moha1379>
2021-02-16 18:36:50 +0330

    if cgen not complete

--- src/CGEN.py
+++ src/CGEN.py
@@ -1,5 +1,5 @@
 from table import init_decls, ecognize_class_functions
-from mipsCodes import emit_comment
+from mipsCodes import emit_comment, create_label, emit_label
 
 def cgen(node):
     emit(".text")
@@ -33,3 +33,17 @@ def find_type(node):
         return temp_type.children[0].children[0].data, len(temp_type.children) - 1
     else :
         return temp_type.children[0].children[0].data
+
+
+
+def cgen_if(node):
+    else_lable = create_label()
+    t1 = cgen_expr(node.children[2])
+    #emit_load() load parameter for jump
+    cgen_stmt(node.children[4])
+    emit_label(else_lable)
+    if node.children[5].children[0].data == "T_ELSE":
+        end_if_lable = create_label()
+        cgen_stmt(node.children[5].children[1])
+        emit_label(end_if_lable)
+    #not complete
